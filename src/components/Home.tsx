7e10cc4f8bee9a0a33e2c2cc87e507773b6ad87b
Simen Hall <simen.hall@geodata.no>
2023-01-30 08:23:07 +0100

    Change splash text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,14 +7,19 @@ function Home() {
   return (
     <div >
       <div className='flex flex-col items-center p-10'>
-       <div className='text-xl'>Heisann!</div>
-       <br/>
-       <div>Jaså, så du har lyst til å komme deg inn i denne kassa? Jaja, det er bare å prøve! Det kan hende at du kommer til å trenge noen "verktøy" som du finner
-         på dette nettstedet. Du finner dem i menyen på toppen av siden. Men det kan selvsagt ikke være så enkelt, først må du finne passordene for å låse opp verktøyene! Psst: Første passord er "<b>passord</b>"
-         Lykke til!
-       </div>
+        <div className='text-xl'>Heisann!</div>
+        <br />
+        <div className='mb-2'>
+          Velkommen til <b>EzScape 0.1</b>. Dette eksperimentelle nettstedet inneholder verktøy for å bryte seg inn i trekasser låst med hengelåser og annet faenskap. Hvis du ikke har en trekasse foran deg anbefaler jeg at du finner på noe annet.
+        </div>
+        <div className='mb-4'>
+          Verktøyene du trenger for å åpne kassa finner du i menyen på toppen av siden, men før du kan bruke dem må du låse dem opp. Passordene består av 6-sifrede tallkoder.
+        </div>
+        <div>
+          Lykke til!
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
 
